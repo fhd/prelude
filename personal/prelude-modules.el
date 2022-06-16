ac1016378ae0ada35dedb14888b00022234c1e79
Felix H. Dahlke <fhd@uberco.de>
2022-06-16 15:00:05 +0200

    Add prelude-common-lisp

--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -67,7 +67,7 @@
 (require 'prelude-c)
 ;; (require 'prelude-clojure)
 ;; (require 'prelude-coffee)
-;; (require 'prelude-common-lisp)
+(require 'prelude-common-lisp)
 (require 'prelude-css)
 ;; (require 'prelude-dart)
 (require 'prelude-emacs-lisp)
